92c8399981ba55b42a0db5a19c2628ca4c2f8d18
Sebastian Budyński <Sebbudynski@gmail.com>
2024-03-05 15:56:44 +0100

    Build everything exept box-shoadow for button

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 "use strict";
 
-fetch("https://api.adviceslip.com/advice")
-  .then((response) => response.json())
-  .then((data) => {
-    document.querySelector(".advice-id").innerHTML = data.slip.id;
-    document.querySelector(".advice").innerHTML = data.slip.advice;
-  });
+const dice = document.querySelector("button");
+
+function fetchAdvice() {
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(
+        ".advice-id"
+      ).innerHTML = `ADVICE #${data.slip.id}`;
+      document.querySelector(".advice").innerHTML = `"${data.slip.advice}"`;
+    });
+}
+dice.addEventListener("click", fetchAdvice);
+
+fetchAdvice();
